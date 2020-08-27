8cc4be60d01214f4c4e8fff8d8d25ab3b4aafb24
BigNut <54455520+Strotai@users.noreply.github.com>
2020-08-27 11:32:09 +0100

    Add files via upload

---
+++ Merge sort Binary Search/Merge sort Binary Search/MergeObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merge_sort_Binary_Search
+{
+    class MergeObj
+    {
+        private uint[] a, b;
+        public MergeObj()
+        {
+            a = new uint[256];
+            b = new uint[256];
+        }
+
+
+        public uint[] A
+        {
+            get { return a; }
+            set { A = value; }  // set method
+        }
+
+        public uint[] B
+        {
+            get { return b; }
+            set { B = value; }  // set method
+        }
+    }
+}
---
+++ Merge sort Binary Search/Merge sort Binary Search/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Transactions;
+
+namespace Merge_sort_Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint[] A = new uint[256];
+            uint[] B = new uint[256];
+            MergeObj mergeBox = new MergeObj();
+            A = EnterUserNumbers();
+            mergeBox = CopyToMergeObjA(mergeBox, A);
+            // A = mergeBox.A;
+            
+            //mergeBox.A[1] = 6;
+            int n = 0;
+            for (int i = 0; i < 256; i++)
+            {
+               // Console.Write(mergeBox.A[i]);
+                if (mergeBox.A[i] != 0)
+                {
+                    n++;
+                    Console.Write(mergeBox.A[i]);
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sorted: ");
+            mergeBox = TopDownMergeSort(A, B, n);
+            for (int i = 0; i < 256; i++)
+            {
+                if (mergeBox.B[i] != 0)
+                {
+                    Console.Write(mergeBox.B[i]);
+                    Console.Write(" ");
+                }
+            }
+        }
+
+        static uint[] EnterUserNumbers()
+        {
+            uint[] text = new uint[256];
+            
+            uint current = 1;
+            int index = 0;
+            while (current != 0)
+            {
+                Console.WriteLine("Press 0 to exit");
+                Console.WriteLine("Size: " +  index);
+                try
+                {
+                    current = (uint)Convert.ToInt32(Console.ReadLine());
+                }
+                catch (InvalidCastException e)
+                {
+                    Console.WriteLine("invalid character");
+                }
+                Console.Clear();
+
+                text[index] = current;
+                index++;
+            }
+
+            return text;
+        } 
+        static MergeObj CopyToMergeObjA(MergeObj mergeBox, uint[] A)
+        {
+            for(int i = 0; i < 256; i++)
+            {
+                mergeBox.A[i] = A[i];
+            }
+            return mergeBox;
+        }
+        static MergeObj CopyToMergeObjB(MergeObj mergeBox, uint[] B)
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                mergeBox.B[i] = B[i];
+            }
+            return mergeBox;
+        }
+
+        static MergeObj TopDownMergeSort(uint[] A, uint[] B, int n)
+        {
+            MergeObj mergeBox = new MergeObj();
+
+            mergeBox = CopyArray(A, 0, n, B);           // one time copy of A[] to B[]
+            A = mergeBox.A;
+            B = mergeBox.B;
+            mergeBox = TopDownSplitMerge(B, 0, n, A);   // sort data from B[] into A[]
+            return mergeBox;
+        }
+
+        static MergeObj CopyArray(uint[] A, int iBegin, int iEnd, uint[] B)
+        {
+            MergeObj mergeBox = new MergeObj();
+            for (int k = iBegin; k < iEnd; k++) { 
+                B[k] = A[k];
+            }
+
+            mergeBox = CopyToMergeObjA(mergeBox, A);
+            mergeBox = CopyToMergeObjB(mergeBox, B);
+            return mergeBox;
+        }
+
+        static MergeObj TopDownSplitMerge(uint[] B, int iBegin, int iEnd, uint[] A)
+        {
+            MergeObj mergeBox = new MergeObj();
+
+            if (iEnd - iBegin <= 1) {
+                mergeBox = CopyToMergeObjA(mergeBox, A);
+                mergeBox = CopyToMergeObjB(mergeBox, B);
+                return mergeBox;
+            }                                                     // if run size == 1
+                                                                  // consider it sorted
+                                                                  // split the run longer than 1 item into halves
+            int iMiddle = (iEnd + iBegin) / 2;                    // iMiddle = mid point
+                                                                  // recursively sort both runs from array A[] into B[]
+
+
+            mergeBox = TopDownSplitMerge(A, iBegin, iMiddle, B);  // sort the left  run
+            A = mergeBox.A;
+            B = mergeBox.B;
+
+            mergeBox = TopDownSplitMerge(A, iMiddle, iEnd, B);    // sort the right run
+                                                                  // merge the resulting runs from array B[] into A[]
+            A = mergeBox.A;
+            B = mergeBox.B;
+            mergeBox = TopDownMerge(B, iBegin, iMiddle, iEnd, A);
+            return mergeBox;
+        }
+
+        static MergeObj TopDownMerge(uint[] A, int iBegin, int iMiddle, int iEnd, uint[] B)
+        {
+            int i = iBegin, j = iMiddle;
+            MergeObj mergeBox = new MergeObj();
+            // While there are elements in the left or right runs...
+            for (int k = iBegin; k < iEnd; k++)
+            {
+                // If left run head exists and is <= existing right run head.
+                if (i < iMiddle && (j >= iEnd || A[i] <= A[j]))
+                {
+                    B[k] = A[i];
+                    i = i + 1;
+                }
+                else
+                {
+                    B[k] = A[j];
+                    j = j + 1;
+                }
+            }
+            mergeBox = CopyToMergeObjA(mergeBox, A);
+            mergeBox = CopyToMergeObjB (mergeBox, B);
+            return mergeBox;
+        }
+    }
+}
